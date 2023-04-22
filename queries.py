b33e560a34ee3426e8f559e7c6c0f8ebec98856e
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2023-04-22 04:21:45 +0300

    all queries working

--- queries.py
+++ queries.py
@@ -1,3 +1,6 @@
+import calendar
+from datetime import datetime
+
 import influxdb_client
 
 def make_queries():
@@ -23,11 +26,65 @@ def make_queries():
         |> range(start: {start_time}, stop: {stop_time}) """
         tables = query_api.query(query, org=org)
 
+        sum = 0
         print(f"\nPäivän {user_date} hinnat tunneittain:")
         for table in tables:
             for record in table.records:
                 print(f"{record['_time'].hour}:00    {round(record['_value'], 3)} ¢")
+                sum += record['_value']
+        print("AVG: " + str(round(sum/24, 3)))
 
+        start_time = user_date[:7] + "-01T00:00:00Z"
+
+        # Lasketaan montako päivää kuukaudessa on
+        date_object = datetime.strptime(user_date, '%Y-%m-%d')
+        days_in_month = calendar.monthrange(date_object.year, date_object.month)[1]
+        stop_time = user_date[:7] + f"-{days_in_month}T23:59:59Z"
+
+        # keskimääräiset päivähinnat kuukausittain
+        query = f"""from(bucket: "e_prices")
+         |> range(start: {start_time}, stop: {stop_time})
+         |> filter(fn: (r) => r["_measurement"] == "electricity_price" and r["_field"] == "price")
+         |> aggregateWindow(every: 1d, fn: mean, createEmpty: false)
+         |> yield(name: "mean")"""
+        tables = query_api.query(query, org=org)
+
+        amount = 0
+        sum = 0
+        print(f"\nKeskimääräiset päivähinnat {user_date[:7]}:")
+        for table in tables:
+            for record in table.records:
+                amount += 1
+                if amount == len(table.records):
+                    print(f"{record['_time'].day}.{record['_time'].month}    {round(record['_value'], 3)} ¢")
+                else:
+                    print(f"{record['_time'].day - 1}.{record['_time'].month}    {round(record['_value'], 3)} ¢")
+                sum += record['_value']
+        print("AVG: " + str(round(sum / days_in_month, 3)))
+
+        start_time = user_date[:4] + "-01-01T00:00:00Z"
+        stop_time = user_date[:4] + "-12-31T00:00:00Z"
+
+        # keskimääräiset kuukausihinnat vuosittain
+        query = f"""from(bucket: "e_prices")
+         |> range(start: {start_time}, stop: {stop_time})
+         |> filter(fn: (r) => r["_measurement"] == "electricity_price" and r["_field"] == "price")
+         |> aggregateWindow(every: 1mo, fn: mean, createEmpty: false)
+         |> yield(name: "mean")"""
+        tables = query_api.query(query, org=org)
+
+        amount = 0
+        sum = 0
+        print(f"\nKeskimääräiset kuukausihinnat {user_date[:4]}:")
+        for table in tables:
+            for record in table.records:
+                amount += 1
+                if amount == len(table.records):
+                    print(f"{record['_time'].month}    {round(record['_value'], 3)} ¢")
+                else:
+                    print(f"{record['_time'].month - 1}    {round(record['_value'], 3)} ¢")
+                sum += record['_value']
+        print("AVG: " + str(round(sum / 12, 3)))
 
     except Exception as e:
         print(e)
@@ -35,4 +92,3 @@ def make_queries():
     finally:
         if client is not None:
             client.close()
-
